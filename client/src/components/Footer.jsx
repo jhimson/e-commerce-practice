b238d46f4b65a6b4486266778e905ba7225cbeb8
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-08-17 07:39:16 -0400

    style: initial design for Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const Footer = () => <footer>Footer</footer>;
+const Footer = () => (
+  <footer>
+    <div className="flex items-center justify-center w-full py-3 bg-red-200">
+      Copyright &copy; JhimShop
+    </div>
+  </footer>
+);
 
 export default Footer;
